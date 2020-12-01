9e06344d6160294690462ac004c6a793c325dacd
CsharpSyntraWestKortrijk <61596529+CSharpSyntraWest@users.noreply.github.com>
2020-12-01 10:58:41 +0100

    Add files via upload

---
+++ Demo_Opdracht3/Demo_Opdracht3/Models/Persoon.cs
@@ -0,0 +1,38 @@
+﻿using Demo_Opdracht3.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo_Opdracht3.Models
+{
+    class Persoon : ObservableObject
+    {
+        private string _voornaam;
+        private string _familienaam;
+        private DateTime _geboorteDatum;
+        public string Voornaam
+        {
+            get { return _voornaam; }
+            set
+            {
+                OnPropertyChanged(ref _voornaam, value);
+            }
+        }
+        public string Familienaam
+        {
+            get { return _familienaam; }
+            set
+            {
+                OnPropertyChanged(ref _familienaam, value);
+            }
+        }
+        public DateTime GeboorteDatum
+        {
+            get { return _geboorteDatum; }
+            set
+            {
+                OnPropertyChanged(ref _geboorteDatum, value);
+            }
+        }
+    }
+ }
