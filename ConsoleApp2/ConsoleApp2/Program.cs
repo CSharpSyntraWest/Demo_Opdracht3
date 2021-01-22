4467b80683e7bdc2c799e1e96d83246790128569
Helena Copppieters <hcoppieters@hotmail.com>
2021-01-22 10:13:59 +0100

    Demos linq queries

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i <=6;i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
